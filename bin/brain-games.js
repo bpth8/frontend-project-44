4597b1a4d4fd585ee20283b37cdab02198cb5442
bpth8 <itmy3denis@gmail.com>
2025-03-27 14:28:21 +0200

    Работа и исправление ошибок с Eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+// eslint-disable-next-line import/extensions
 import userName from '../src/cli.js';
 
 userName();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import readlineSync from 'readline-sync';
 
 const userName = () => {
+  // eslint-disable-next-line no-console
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
+  // eslint-disable-next-line no-console
   console.log(`Hello, ${name}!`);
 };
 
